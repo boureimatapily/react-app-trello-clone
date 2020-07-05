4a9efad5cd80478a97f82ae8cd813704b976bc38
unknown <boureimatapily1@gmail.com>
2020-07-05 19:05:44 +0000

    ADD DragDropContext

--- src/Pages/Trello.js
+++ src/Pages/Trello.js
@@ -1,35 +1,49 @@
-import React from 'react'
+import React from "react";
 import { withStyles } from "@material-ui/styles";
-import { Grid, Container } from '@material-ui/core';
-import TrelloList from '../Components/Trello/TrelloList';
-import { connect } from 'react-redux';
-import TrelloActionButton from '../Components/Trello/TrelloActionButton';
+import { Grid, Container } from "@material-ui/core";
+import TrelloList from "../Components/Trello/TrelloList";
+import { connect } from "react-redux";
+import TrelloActionButton from "../Components/Trello/TrelloActionButton";
+import {DragDropContext } from 'react-beautiful-dnd';
+
 
 
 const styles = {
-    root:{
-        display: "flex"
-    },
-}
+  root: {
+    display: "flex",
+  },
+};
 
-class Trello extends React.Component{
- 
-  render(){
-     const { classes, lists } = this.props;
-    return(
-        <div> 
-            <h2>hello Codetrain</h2>
-            <div container className={classes.root}>
-                 {lists.map(list => <TrelloList key={list.id} title={list.title} cards={list.cards} />)}
-                 <TrelloActionButton list />
-             </div>
-               
+class Trello extends React.Component {
+
+  onDragEnd = () =>{
+
+  }
+
+  render() {
+
+    const { classes, lists } = this.props;
+
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}> 
+      <div>
+        <h2>hello Codetrain</h2>
+        <div container className={classes.root}>
+          {lists.map((list) => (
+            <TrelloList key={list.id} 
+             listID={list.id}
+             title={list.title} 
+             cards={list.cards} />
+          ))}
+          <TrelloActionButton list />
         </div>
-    )
+      </div>
+      </DragDropContext>
+    );
   }
 }
-const mapStateToprops = state =>({
-            lists: state.lists
-})
+const mapStateToprops = (state) => ({
+  lists: state.lists,
+});
 
-export default  withStyles(styles)(connect(mapStateToprops)(Trello))
+export default withStyles(styles)(connect(mapStateToprops)(Trello));
