a7dfe9f5011a71bc0963de57d5be8e2a4d81be76
unknown <boureimatapily1@gmail.com>
2020-07-05 19:06:30 +0000

    MOD parenthesis to curley braces

---
+++ src/redux/Actions/index.js
@@ -0,0 +1,26 @@
+export * from "./cardsActions";
+export * from "./listActions";
+// export * from "./boardActions";
+
+export const CONSTANTS = {
+    ADD_CARD: "ADD_CARD",
+    ADD_LIST: "ADD_LIST",
+    DRAG_HAPPENED: "DRAG_HAPPENED",
+    EDIT_CARD: "EDIT_CARD",
+    DELETE_CARD: "DELETE_CARD",
+    EDIT_LIST_TITLE: "EDIT_LIST_TITLE",
+    DELETE_LIST: "DELETE_LIST",
+    SET_ACTIVE_BOARD: "SET_ACTIVE_BOARD",
+    ADD_BOARD: "ADD_BOARD"
+  };
+  
+
+// export const LOGIN_REQUEST = "LOGIN_REQUEST";
+// export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+// export const LOGIN_FAILURE = "LOGIN_FAILURE";
+// export const LOGOUT_REQUEST = "LOGOUT_REQUEST";
+// export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+// export const LOGOUT_FAILURE = "LOGOUT_FAILURE";
+// export const VERIFY_REQUEST = "VERIFY_REQUEST";
+// export const VERIFY_SUCCESS = "VERIFY_SUCCESS";
+// export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
